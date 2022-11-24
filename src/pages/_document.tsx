d59a9e6b42db94cfb22d2d1ecef1e9affc97cd57
Breno Castelo Branco <breno.cbsousa@gmail.com>
2022-11-24 10:38:29 +0000

    setting up the app

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <body>
+        {/* All the app pages goes in the Main component */}
+        <Main />
+
+        {/* Where the Next App script should be loaded*/}
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>AvGeek Store</h1>;
+}
