5041f59910e37ba6259a0743b5db833e0977f7a4
Breno Castelo Branco <breno.cbsousa@gmail.com>
2022-11-30 09:20:03 +0000

    fetching data and generating dynamic static pages for product details page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,8 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Stripe from 'stripe';
+import { stripe } from '../../lib/stripe';
 import {
   DetailsContainer,
   ImageContainer,
@@ -7,30 +10,69 @@ import {
 } from '../../styles/pages/product';
 import { formatPrice } from '../../utils/format-price';
 
-export default function Product() {
-  const { query } = useRouter();
+type Props = {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+};
 
+export default function Product({ product }: Props) {
   return (
     <ProductContainer>
       <ImageContainer>
-        <Image
-          src="http://localhost:3000/_next/image?url=https%3A%2F%2Ffiles.stripe.com%2Flinks%2FMDB8YWNjdF8xTTd6TE9KVGNJVmVpUVgyfGZsX3Rlc3RfcGliY0tWVDhPdEI5QTNHUkRKRUx1d0JI00y5Pqxssr&w=384&q=75"
-          alt=""
-          width={320}
-          height={320}
-        />
+        <Image src={product.imageUrl} alt="" width={320} height={320} />
       </ImageContainer>
       <DetailsContainer>
-        <h1>Camisa x</h1>
-        <span>{formatPrice(79)}</span>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum
-          deserunt soluta at, aliquam eum et accusantium. Quos veniam omnis
-          doloremque, atque rem, sequi eligendi sit dolores provident
-          consequuntur, necessitatibus quam!
-        </p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
+
         <button>Comprar agora</button>
       </DetailsContainer>
     </ProductContainer>
   );
+}
+
+export async function getStaticPaths() {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  });
+
+  return {
+    paths: response.data.map((product) => {
+      return {
+        params: { id: product.id },
+      };
+    }),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({
+  params,
+}: GetStaticPropsContext<{ id: string }>) {
+  if (!params) return;
+
+  const product = await stripe.products.retrieve(params.id, {
+    expand: ['default_price'],
+  });
+
+  const { unit_amount } = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        // formatting price on static site generation, so tha it only runs once
+        price: formatPrice(unit_amount ? unit_amount / 100 : 0),
+      },
+      revalidate: 60 * 60 * 1,
+    },
+  };
 }
